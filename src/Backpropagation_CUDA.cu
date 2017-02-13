4508ce8d9f739999e34629c219dcd5e274777aa3
Nikos-T <nikos.terzo@gmail.com>
2017-02-13 17:58:53 +0200

    parser

--- src/Backpropagation_CUDA.cu
+++ src/Backpropagation_CUDA.cu
@@ -9,7 +9,25 @@ int main(int argc, char **argv) {
 *	
 *	
 */
+unsigned int L = atoi(argv[1]), *layer_sizes = (unsigned int *)malloc(L*sizeof(unsigned int));
 
+if (layer_sizes==NULL) {
+	printf("Could not allocate memory to layer_sizes.\nExiting...\n");
+	return -1;
+}
+
+if (argc != L+2) {
+	printf("Usage:\n./name_of_program L l1 l2 l3 ... lL\nWhere\nL is the length of the neural network\nand\nl1 l2 l3 ... lL the size of the corresponding layer.\nExiting...");
+	return 1;
+}
+
+for (int i=2; i<argc; i++) {
+	layer_sizes[i-2] = atoi(argv[i]);
+}
+
+for (int i=0; i<L; i++) {
+	printf("%i\n", layer_sizes[i]);
+}
 
 
 
